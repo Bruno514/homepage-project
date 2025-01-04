64f3b2a616403998f25dd562fb8efea8fe5f4c49
Bruno Oliveira <86791361+Bruno514@users.noreply.github.com>
2025-01-04 09:06:33 -0300

    fix 'my-work' section grid, add project image colors

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+const colors = [
+  "#7C75CA",
+  "#D5D5D5",
+  "#5E8F4D",
+  "#CA7599",
+  "#4692D9",
+  "#5E8F4D",
+];
+
 const projectsElement = document.getElementsByClassName("projects")[0];
 
 for (let index = 0; index < 5; index++) {
   const projectNode = document.getElementsByClassName("project")[0];
+  const projectImage = projectNode.querySelector(".project-image");
+  projectImage.style.backgroundColor = colors[index];
   const clone = projectNode.cloneNode(true);
 
   projectsElement.appendChild(clone);
